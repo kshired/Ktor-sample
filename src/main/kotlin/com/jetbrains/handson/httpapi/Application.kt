27111ca291d97bfd14a53d810cd017d458a54962
kshired <shiroed1211@naver.com>
2022-02-03 01:15:26 +0900

    [ADD] CORS

--- src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
@@ -4,12 +4,18 @@ import com.jetbrains.handson.routes.registerCustomerRoutes
 import com.jetbrains.handson.routes.registerOrderRoutes
 import io.ktor.application.*
 import io.ktor.features.*
+import io.ktor.http.HttpHeaders
 import io.ktor.serialization.*
 
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module(testing: Boolean = false) {
+    install(CORS) {
+        host("*")
+        header(HttpHeaders.ContentType)
+    }
+
     install(ContentNegotiation) {
         json()
     }
